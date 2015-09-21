155ad6390d48014d90eedba7dd9be49b220eb066
Jack Zhang <moonboots08@gmail.com>
2015-09-20 17:32:05 -0700

    Add hammerspoon init.lua

---
+++ .hammerspoon/init.lua
@@ -0,0 +1,62 @@
+-- http://www.hammerspoon.org/docs/hs.mjomatic.html
+
+local hyper = {"cmd", "alt", "ctrl", "shift"}
+
+hs.hotkey.bind(hyper, "h", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hyper, "l", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x + (max.w / 2)
+  f.y = max.y
+  f.w = max.w / 2
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+hs.hotkey.bind(hyper, "F", function()
+  local win = hs.window.focusedWindow()
+  local f = win:frame()
+  local screen = win:screen()
+  local max = screen:frame()
+
+  f.x = max.x
+  f.y = max.y
+  f.w = max.w
+  f.h = max.h
+  win:setFrame(f)
+end)
+
+function reloadConfig(files)
+    doReload = false
+    for _,file in pairs(files) do
+        if file:sub(-4) == ".lua" then
+            doReload = true
+        end
+    end
+    if doReload then
+        hs.reload()
+    end
+end
+hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
+
+hs.pathwatcher.new(os.getenv("HOME") .. "/Library/Application Support/Karabiner/private.xml", function()
+	hs.execute("/Applications/Karabiner.app/Contents/Library/bin/karabiner reloadxml")
+	hs.notify.new({title="Hammerspoon", informativeText="private.xml changed"}):send():release()
+end):start()
+
+hs.notify.new({title="Hammerspoon", informativeText="Config loaded"}):send():release()
