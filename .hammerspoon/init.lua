6b0ea12ce26fd1725fc7e48ec64503cb1b4e0e37
Jack Zhang <moonboots08@gmail.com>
2015-09-22 12:05:15 -0700

    fix indent

--- .hammerspoon/init.lua
+++ .hammerspoon/init.lua
@@ -57,15 +57,15 @@ hs.hotkey.bind(hyper, "3", function()
 end)
 
 function reloadConfig(files)
-    doReload = false
-    for _,file in pairs(files) do
-        if file:sub(-4) == ".lua" then
-            doReload = true
-        end
-    end
-    if doReload then
-        hs.reload()
-    end
+  doReload = false
+  for _,file in pairs(files) do
+      if file:sub(-4) == ".lua" then
+          doReload = true
+      end
+  end
+  if doReload then
+      hs.reload()
+  end
 end
 hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
