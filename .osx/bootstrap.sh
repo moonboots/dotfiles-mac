8227a263c9313efe3580593e6ced6246bb0668a0
Jack Zhang <moonboots08@gmail.com>
2015-09-26 12:25:26 -0700

    Start bootstrap.sh

---
+++ .osx/bootstrap.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+set -eu
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew cask install \
+  karabiner \
+  seil \
+  hammerspoon
+
+seil=/Applications/Seil.app/Contents/Library/bin/seil
+$seil set keycode_capslock 80
+$seil relaunch
+
+brew install \
+  tmux \
+  vim
